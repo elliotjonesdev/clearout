36d9d69e6cec256db193e500dac9202cf25b035f
elliotjonesdev <elliotjonesdev@gmail.com>
2023-08-17 17:08:45 +0000

    update spacing on app.py file

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello():
     return "Hello World ... again!"
@@ -16,3 +15,4 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+            
